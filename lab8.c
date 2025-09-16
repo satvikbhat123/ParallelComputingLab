fba19a595a72e0fb99355b7df71e82ca4f53c514
satvikbhat123 <satvik.22cs139@sode-edu.in>
2025-09-16 11:10:47 +0530

    lab

--- lab8.c
+++ lab8.c
@@ -2,17 +2,21 @@
 #include <mpi.h>
 
 int main(int argc, char** argv) {
-    int rank, data = 0;
+    int rank, size, data[4] = {10, 20, 30, 40}, recv;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (rank == 0)
-        data = 100;
+    MPI_Scatter(data, 1, MPI_INT, &recv, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    recv += 1;
+    MPI_Gather(&recv, 1, MPI_INT, data, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    MPI_Bcast(&data, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    printf("Process %d received data: %d\n", rank, data);
+    if (rank == 0) {
+        printf("Gathered data: ");
+        for (int i = 0; i < size; i++) printf("%d ", data[i]);
+        printf("\n");
+    }
 
     MPI_Finalize();
     return 0;
